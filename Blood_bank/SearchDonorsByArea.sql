92e0660e65f5602635c650a59e138cd0d6ba004b
momenshovon <30320290+momenshovon@users.noreply.github.com>
2019-10-12 02:29:19 +0600

    Update SearchDonorsByArea.sql

--- Blood_bank/SearchDonorsByArea.sql
+++ Blood_bank/SearchDonorsByArea.sql
@@ -1,26 +1,27 @@
-create or replace procedure searchDonorsByArea(matchArea in DONOR.Darea%TYPE)
+	create or replace procedure searchDonorsByArea(matchArea in DONOR.Darea%TYPE)
 		IS
     id Donor.DID%Type;	
-	name varchar2(100);
+	name Donor.Dname%TYPE;
 	age DONOR.Dage%TYPE;
-	gender varchar2(100);
-	bloodGrp varchar2(100);
-	area varchar2(100);
-	address varchar2(100);
-	phonenum varchar2(100);
-	elgib varchar2(100);
+	gender Donor.Dgender%TYPE;
+	bloodGrp Donor.Dbloodgroup%TYPE;
+	area Donor.Darea%TYPE;
+	address Donor.Daddress%TYPE;
+	phonenum Donor.Dphonenum%TYPE;
+	elgib Donor.Deligibility%TYPE;
 	
 	
    cursor donorbyarea is
-	select * from DONOR
+	select did, dname, dage, dgender, dbloodgroup, darea, daddress, dphonenum, Deligibility from DONOR
 	where Darea = matchArea;
 	
  begin	
 	
-	DBMS_OUTPUT.PUT_LINE('DID'||' -- '||'Name'||' -- ' || 'Age'||' -- ' || 'Gender' ||' -- '|| 'Blood Group'||' -- '|| 'Area' ||' -- '|| 'Address' ||' -- '|| 'Phone Number' ||' -- '|| 'Eligibility');
+	DBMS_OUTPUT.PUT_LINE('DID -- Name -- Age -- Gender -- Blood Group -- Area -- Address -- Phone Number -- Eligibility');
 	
 	open donorbyarea;
 	loop
+		
 		fetch donorbyarea into id,name,age,gender,bloodGrp,area,address,phonenum,elgib;
 		
 		exit when donorbyarea%notfound;
